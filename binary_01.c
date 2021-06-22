3cabfc3bc9fbe2a313664d86a719cd127e250726
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2021-06-22 23:33:48 +0800

    added a file

---
+++ binary_01.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include"stdio.h"
+#include"math.h"
+
+/*
+int bin(int n)
+{
+	int i = 0;
+	printf("%d", n);
+}
+
+
+int main()
+{
+	count3_bit_one(5);
+	return 0;
+}
+
+
+int count3_bit_one(int n)
+{
+	int i = 0;
+	int count = 0;
+	for (i = 31; i >=0; i--)
+	{
+		int j = 0;
+		j = ((n >> i) & 1);
+		printf("%d", j);	
+	}
+
+}
+
+*/
