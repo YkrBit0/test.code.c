0a697974addb261ddb57bd393beff789d979c44f
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2022-10-30 21:14:44 +0800

    add a file

---
+++ test_24.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+
+    // float f = 5.5f;
+    // float* p = &f;
+    //101.1
+    //1.011*2^2
+    //s = 0 m = 1.011 E = 2
+    //s = 0 m = 011 E = 2+127
+    //0100 0000 1011 0000 0000 0000 0000 0000
+    //40 b0 00 00 
+    // printf("%p\n",p);
+    // printf("也无任何唯一");
+    char* ps = "hello bit";
+    // char t[] = "sdfgdg";
+    // printf("%c\n",t);
+
+
+    printf("%c\n",*ps);
+
+
+    int d = 10;
+
+
+    return 0;
+}
+
+
+
