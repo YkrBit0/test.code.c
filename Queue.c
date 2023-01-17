2b75f7a3449a6fe3d785a56c6150127dea5b4533
YkrBit0 <83524483+YkrBit0@users.noreply.github.com>
2023-01-17 20:08:35 +0800

    add a file

---
+++ Queue.c
@@ -0,0 +1,30 @@
+#include "Queue.h"
+
+
+//³õÊ¼»¯
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+	pq->head = NULL;
+	pq->tail = NULL;
+}
+
+void QueueDestroy(Queue* pq)
+{
+	assert(pq);
+	QueueNode* cur = pq->head;
+	while (cur != NULL)
+	{
+		QueueNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	pq->head = pq->tail = NULL;
+}
+
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+int QueueSize(Queue* pq);
+bool QueueEmpty(Queue* pq);
